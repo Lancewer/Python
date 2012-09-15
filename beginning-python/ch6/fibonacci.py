24c11b2670ab5ce47474547e713cb0679ad06f80
Lancewer <lancewer@gmail.com>
2012-09-15 12:00:23 +0800

    add comment

--- beginning-python/ch6/fibonacci.py
+++ beginning-python/ch6/fibonacci.py
@@ -1,5 +1,6 @@
 """
 This is a method to calculate fibonacci list from chapter 6
+This is a good way to calculate fibonacci number
 """
 fibs = [0,1]
 num = input('How many fibonacci numbers do you want?')
