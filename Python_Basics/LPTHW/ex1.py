64452590847fc7fcdfe4ccad7ac837aeceec5fe6
Lancewer <Lancewer@gmail.com>
2012-12-25 22:47:59 +0800

    add learn python the hard way codes

---
+++ Python_Basics/LPTHW/ex1.py
@@ -0,0 +1,12 @@
+#  -*- coding: utf-8 -*- this line enable unicode
+#This exercise file is the first practice from the book, which is to show how to use print statement.
+__author__ = 'Lancewer'
+
+print "Hello World"
+print "Hello Again"
+print  "I like typing this"
+print  "This is fun"
+print  "Yay! Printing"
+print  "I'd much rather you 'not'"
+print  'I "said" do not touch this.'
+print "这是一行中文"
---
+++ Python_Basics/LPTHW/ex3.py
@@ -0,0 +1,60 @@
+__author__ = 'Lancewer'
+__doc__ = "This file is for exercise 3 from the book -- use math"
+
+print "I will now count my chickens:"
+
+print "Hens:", 25 + 30 / 6
+print  "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print  3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "1 / 4 = ", 1 / 4
+print "4 % 2 =", 4 % 2
+
+print "Is is true that 3 + 2 < 5 - 7?"
+
+print  3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is  5 - 7?", 5 - 7
+
+print  "Oh, that's why it's False."
+
+print  "How about same more."
+
+print "Is it greater?", 5 > -2
+print  "Is it greater or equal?", 5 >= -2
+print  "Is it less or equal?", 5 <= -2
+
+#float version of upper codes
+print "\n-----This is float version------"
+print "I will now count my chickens:"
+
+print "Hens:", 25 + 30.0 / 6
+print  "Roosters", 100 - 25 * 3 % 4.0
+
+print "Now I will count the eggs:"
+
+print  3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + 6
+print "1 / 4 = ", 1 / 4.0
+print "4 % 2 =", 4 % 2.0
+
+print "Is is true that 3 + 2 < 5 - 7?"
+
+print  3 + 2.0 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2.0
+print "What is  5 - 7?", 5 - 7.0
+
+print  "Oh, that's why it's False."
+
+print  "How about same more."
+
+print "Is it greater?", 5 > -2.0
+print  "Is it greater or equal?", 5 >= -2.0
+print  "Is it less or equal?", 5 <= -2.0
+
+#Additional exercises
+print "The water boils at: ", 32 + 100.0 * 9 / 5, "Fahrenheit."
+print "The water boils at: ", (212.0 - 32) * 5.0 / 9, "Celsius."
